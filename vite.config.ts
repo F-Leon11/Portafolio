abf634f50e2a5f9eebfa343500a92235401f632c
FelipeL11 <jfelipeleon11@gmail.com>
2025-01-15 15:18:20 -0500

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/F-Leon11.github.io/",
+  base: "https://F-Leon11.github.io/F-Leon11.github.io",
 });
