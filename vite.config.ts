663943bc498b4f20475f629abef56142088aeed9
FelipeL11 <jfelipeleon11@gmail.com>
2025-01-15 18:44:33 -0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/Portafolio/" : "/",
+  base: "https://F-leon11.github.io/Portafolio",
 });
